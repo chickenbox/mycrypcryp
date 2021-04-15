57c350f2ada9ad88e9bfd655af9d904efe1a61bc
anmc <lamylanmc@hotmail.com>
2021-04-15 09:30:46 +0800

    move tmp code to src

--- tmp/config.ts
+++
@@ -1,9 +0,0 @@
-namespace hahaApp {
-
-    export const config = {
-        version: "1.0.4",
-        author: "ANMCダンボールとり",
-        projectUrl: "https://github.com/danborutori/Softbody-Demo"
-    }
-
-}
